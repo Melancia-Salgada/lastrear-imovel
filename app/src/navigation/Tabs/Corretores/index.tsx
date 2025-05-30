10d80b23160473ae5800457a49c26d050fcc8818
Gabriel Almeida <GabrielAlmeida.dev@outlook.com>
2025-05-30 15:37:59 -0300

    fix: retirada bottom sheet

--- app/src/navigation/Tabs/Corretores/index.tsx
+++ app/src/navigation/Tabs/Corretores/index.tsx
@@ -2,9 +2,7 @@ import HeaderSearch from '@/app/src/components/HeaderSearch'
 import TemplateNavScreen from '@/app/src/components/TemplateNavScreen'
 import React, { useMemo } from 'react'
 import { useRouter } from 'expo-router'
-import BottomSheet from '@gorhom/bottom-sheet'
-import { Text, View } from 'react-native'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
 
 function Corretores() {
 
