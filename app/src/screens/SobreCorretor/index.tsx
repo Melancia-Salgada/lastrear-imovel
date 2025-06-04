7544bd54f4dd647aa339ad34d84fde0f88fb5cd3
Kauansddsdela <116198015+Kauansddsdela@users.noreply.github.com>
2025-06-04 15:29:01 -0300

    sobre corretor

--- app/src/screens/SobreCorretor/index.tsx
+++ app/src/screens/SobreCorretor/index.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import HeaderMais from '../../components/HeaderMais'
 import { useRouter } from 'expo-router'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import Title from '../../components/Title'
+import Texto from '../../components/Texto'
 
 function SobreCorretor() {
 
@@ -14,8 +16,49 @@ function SobreCorretor() {
   
   return (
     <SafeAreaView style={styles.novo}>
-      
       <HeaderMais handleClickClose={handleClose} editar/>
+      <View style={styles.sobreClienteArea}>
+        <Title style={styles.sobreClienteNome}>
+          Gabriel Almeida Ferreira
+      </Title>
+      <Texto style={styles.sobreClienteLabel}>
+        Email
+      </Texto>
+      <Texto>
+        exemplo@exemplo.com
+      </Texto>
+      <Texto style={styles.sobreClienteLabel}>
+        Telefone
+      </Texto>
+      <Texto>
+       2131323416
+      </Texto>
+      <Texto style={styles.sobreClienteLabel}>
+        CPF
+      </Texto>
+      <Texto>
+        123.456-78
+      </Texto>
+      <Texto style={styles.sobreClienteLabel}>
+        Data de nascimento
+      </Texto>
+      <Texto>
+        12/12/2021
+      </Texto>
+      <Texto style={styles.sobreClienteLabel}>
+        Especialidade
+      </Texto>
+      <Texto>
+        corretor 
+      </Texto>
+      <Texto style={styles.sobreClienteLabel}>
+        Data de cadastro
+      </Texto>
+      <Texto>
+        12/12/2021
+      </Texto>
+      </View>
+      
     </SafeAreaView>
   )
 }
@@ -26,5 +69,18 @@ const styles = StyleSheet.create({
   novo: {
     backgroundColor: '#fff',
     flex: 1
-  }
+  },
+   sobreClienteArea: {
+    paddingHorizontal:16,
+    flex: 1, 
+  },
+  sobreClienteNome: {
+    fontWeight: 'bold',
+    marginBottom: 15,
+    paddingTop: 16, 
+  },
+  sobreClienteLabel: {
+    fontWeight: 'bold',
+    marginVertical: 3
+  },
 })
